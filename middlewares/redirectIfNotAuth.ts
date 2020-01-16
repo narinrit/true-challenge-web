dad07cd0df4278e5dfe92d3935a4d10ff1f2818e
Narinrit <ifra@vista.aero>
2020-01-17 02:55:13 +0700

    Remove unused code

--- middlewares/redirectIfNotAuth.ts
+++ middlewares/redirectIfNotAuth.ts
@@ -3,18 +3,12 @@ import { NextPageContextWithStore } from '../store/types';
 
 export default function redirectIfNotAuth({ store, req, res }: NextPageContextWithStore) {
     const { auth } = store.getState();
-    let url;
 
     if (auth.isAuth) {
-        // eslint-disable-next-line no-constant-condition
-        if (auth.user.reset_password) {
-            url = '/change-password';
-        } else {
-            return;
-        }
-    } else {
-        url = '/sign-in';
+        return;
     }
+
+    const url = '/sign-in';
 
     if (res) {
         if (req.url === url) return;
