b73941d92dbe12d7243b468c25c574aaa7493d12
Narinrit <ifra@vista.aero>
2020-01-17 00:24:40 +0700

    Update index page centent

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,15 @@ import { NextPageContextWithStore } from '../store/types';
 const IndexPage: NextPage = () => (
     <DefaultLayout>
         <Typography className="page-title" variant="h2" gutterBottom>True full-stack challenge web admin</Typography>
-        <Typography variant="body1">Click menu on the left.</Typography>
+        <Typography variant="body1">
+            This project was created for the challenge:
+            <br />
+            <a href="https://rightman.true-e-logistics.com/full-stack/fullstack-challenge.html">
+                https://rightman.true-e-logistics.com/full-stack/fullstack-challenge.html
+            </a>
+        </Typography>
+        <br />
+        <Typography variant="body1">Please click menu on the left.</Typography>
     </DefaultLayout>
 );
 
